39d86f62870bf17a8aa883b8291a75cbbe146a48
PersiLiao <xiangchu.liao@gmail.com>
2020-04-21 02:32:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Envrion IDE Helper
+
+Add IDE helper for the **environ**
+
+## Install
+
+You can add it by `composer`:
+
+```bash
+# use latest code
+composer require persiliao/environ-ide-helper
+```
+
+## LICENSE
+
+See [LICENSE](LICENSE)
---
+++ src/Environ.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace PersiLiao;
+
+/**
+ * Class Environ
+ * @package PersiLiao
+ */
+class Environ
+{
+    /**
+     * Return php.ini -> environ.name
+     *
+     * @return string
+     */
+    public static function getName(): string
+    {
+
+    }
+
+    /**
+     * Return php.ini -> environ.location
+     *
+     * @return string
+     */
+    public static function getLocation(): string
+    {
+
+    }
+
+    /**
+     * Check if environ.name is production
+     *
+     * @return bool
+     */
+    public static function isProduction(): bool
+    {
+
+    }
+
+    /**
+     * Check if environ.name is gray
+     *
+     * @return bool
+     */
+    public static function isGray(): bool
+    {
+
+    }
+
+    /**
+     * Check if environ.name is prerelease
+     *
+     * @return bool
+     */
+    public static function isPreRelease(): bool
+    {
+
+    }
+
+    /**
+     * Check if environ.name is test
+     *
+     * @return bool
+     */
+    public static function isTest(): bool
+    {
+
+    }
+
+    /**
+     * Check if environ.name is development
+     *
+     * @return bool
+     */
+    public static function isDevelopment(): bool
+    {
+
+    }
+
+    /**
+     * Check if environ.name is local
+     *
+     * @return bool
+     */
+    public static function isLocal(): bool
+    {
+
+    }
+}
